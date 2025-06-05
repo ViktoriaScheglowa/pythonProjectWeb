e937f3d8a6ba150a34b5725f26bd22462e09e5bf
Viktory <mokshan.os@mail.ru>
2025-06-05 21:46:48 +0300

    исправление ошибок

--- get_open.py
+++ get_open.py
@@ -1,36 +1,69 @@
-# Импорт встроенной библиотеки для работы веб-сервера
+import json
+import os.path
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
 
 # Для начала определим настройки запуска
-hostName = "localhost" # Адрес для доступа по сети
-serverPort = 8080 # Порт для доступа по сети
+hostName = "localhost"  # Адрес для доступа по сети
+serverPort = 8080  # Порт для доступа по сети
 
-class MyServer(BaseHTTPRequestHandler):
+
+class Server(BaseHTTPRequestHandler):
     """
         Специальный класс, который отвечает за
         обработку входящих запросов от клиентов
     """
+    base_dir = os.path.dirname(__file__)
+
+    urls_path = {
+        "GET":
+            {
+                "/catalog": base_dir + "catalog.html",
+                "/main": base_dir + "main.html",
+                "/category": base_dir + "category.html",
+                "/contact": base_dir + "contact.html",
+                "default": base_dir + "contact.html",
+            },
+        "POST": {
+            "/add_message": {"message": "success"}
+        }
+    }
+
+    def do_POST(self):
+        """Обрабатываем данные по методу POST"""
+        urls_path = self.urls_path["POST"]
+        if self.path in urls_path:
+            content_length = int(self.headers['Content-Length'])
+            user_data = self.rfile.read(content_length).decode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+
+            print(user_data)
+            self.wfile.write(json.dumps(urls_path[self.path]).encode("utf-8"))
+
     def do_GET(self):
         """ Метод для обработки входящих GET-запросов """
-        self.send_response(200) # Отправка кода ответа
-        self.send_header("Content-type", "application/json") # Отправка типа данных, который будет передаваться
-        self.end_headers() # Завершение формирования заголовков ответа
-        self.wfile.write(bytes("{'message': 'OK'}", "utf-8")) # Тело ответа
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        urls_path = self.urls_path["GET"]
+        if self.path in urls_path:
+            template = urls_path[self.path]
+        else:
+            template = urls_path["default"]
+
+        with open(template, encoding="utf-8", mode="r") as f:
+            self.wfile.write(bytes(f.read(), "utf-8"))
+
 
 if __name__ == "__main__":
-    # Инициализация веб-сервера, который будет по заданным параметрах в сети
-    # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
-    webServer = HTTPServer((hostName, serverPort), MyServer)
+    webServer = HTTPServer((hostName, serverPort), Server)
     print("Server started http://%s:%s" % (hostName, serverPort))
 
     try:
-        # Cтарт веб-сервера в бесконечном цикле прослушивания входящих запросов
         webServer.serve_forever()
     except KeyboardInterrupt:
-        # Корректный способ остановить сервер в консоли через сочетание клавиш Ctrl + C
         pass
 
-    # Корректная остановка веб-сервера, чтобы он освободил адрес и порт в сети, которые занимал
     webServer.server_close()
     print("Server stopped.")
