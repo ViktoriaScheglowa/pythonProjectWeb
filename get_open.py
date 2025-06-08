48ad14913bfc25a53ea7a5bb491da2a0b5437833
Viktory <mokshan.os@mail.ru>
2025-06-08 08:55:34 +0300

    исправление ошибок

--- get_open.py
+++ get_open.py
@@ -1,6 +1,6 @@
-import json
 import os.path
 from http.server import BaseHTTPRequestHandler, HTTPServer
+
 
 # Для начала определим настройки запуска
 hostName = "localhost"  # Адрес для доступа по сети
@@ -13,8 +13,17 @@ class MyServer(BaseHTTPRequestHandler):
         Специальный класс, который отвечает за
         обработку входящих запросов от клиентов
     """
-    #base_dir = os.path.dirname(__file__)
-
+    base_dir = os.path.dirname(__file__)
+    path_to_file = {
+        "GET":
+            {
+                "/contact": "contact.html",
+                "/category": "category.html",
+                "/catalog": "catalog.html",
+                "/main": "main.html",
+                "/default": "contact.html"
+            }
+    }
 
     def do_POST(self):
         """Метод для обработки входящих POST-запросов"""
@@ -30,7 +39,14 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("{'message': 'OK'}", "utf-8"))
+        path_to_file = self.path_to_file["GET"]
+        if self.path in path_to_file:
+            template = path_to_file[self.path]
+        else:
+            template = path_to_file["/default"]
+
+        with open(template, encoding="utf-8", mode="r") as f:
+            self.wfile.write(bytes(f.read(), "utf-8"))
 
 
 if __name__ == "__main__":
