fc0925d967c777682b9d0233673413118da4ad44
Viktory <mokshan.os@mail.ru>
2025-06-16 22:49:54 +0300

    выполнение заданий 1 и 2

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+    description = models.CharField(max_length=150, verbose_name='Описание')
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'книга'
+        verbose_name_plural = 'книги'
+        ordering = ['title']
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.CharField(max_length=150, verbose_name='Описание')
+    picture = models.ImageField(upload_to='photos/', verbose_name='Изображение')
+    price = models.IntegerField()
+    created_at = models.DateField(verbose_name='Дата создания')
+    updated_at = models.DateField(verbose_name='Дата последнего изменения')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='продукт')
+
+    def __str__(self):
+        return f'{self.name} {self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ['name']
