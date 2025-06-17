28a96dceff28a87dc8023649ce793d6452507081
Viktory <mokshan.os@mail.ru>
2025-06-17 23:06:58 +0300

    выполнение задания 3 4

--- catalog/models.py
+++ catalog/models.py
@@ -3,25 +3,25 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название')
-    description = models.CharField(max_length=150, verbose_name='Описание')
+    description = models.TextField(blank=True, verbose_name='Описание')
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'книга'
-        verbose_name_plural = 'книги'
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
         ordering = ['title']
 
 
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='Наименование')
-    description = models.CharField(max_length=150, verbose_name='Описание')
+    description = models.TextField(blank=True, verbose_name='Описание')
     picture = models.ImageField(upload_to='photos/', verbose_name='Изображение')
     price = models.IntegerField()
     created_at = models.DateField(verbose_name='Дата создания')
     updated_at = models.DateField(verbose_name='Дата последнего изменения')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='продукт')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='категория')
 
     def __str__(self):
         return f'{self.name} {self.price}'
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import home, contact
@@ -5,6 +6,7 @@ from catalog.views import home, contact
 
 app_name = CatalogConfig.name
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('contact/', contact, name='contact'),
 ]
