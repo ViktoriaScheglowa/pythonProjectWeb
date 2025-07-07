5c16e77b06586b018eeaa830ad620953418f835c
Viktory <mokshan.os@mail.ru>
2025-07-08 00:59:27 +0300

    выполнение домашнего задания 27 п3

--- users/views.py
+++ users/views.py
@@ -1,15 +1,18 @@
 import secrets
 
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse, reverse_lazy
 from django.views import View
-from django.views.generic import CreateView
+from django.views.generic import CreateView, FormView, UpdateView
 
+from catalog.form import ProductForm
+from catalog.models import Product
 from config.settings import EMAIL_HOST_USER
-from users.forms import UserRegisterForm
+from users.forms import UserRegisterForm, CustomUserCreationForm
 from users.models import CustomUser
 
 
@@ -19,13 +22,12 @@ class CustomLogoutView(View):
         return HttpResponseRedirect(reverse('catalog:product_list'))
 
 
-class UserCreateView(CreateView):
-    model = CustomUser
-    form_class = UserRegisterForm
-    success_url = reverse_lazy("users:login")
+class UserCreateView(LoginRequiredMixin, CreateView):
+    template_name = 'users/user_form.html'
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('catalog:product_list')
 
     def form_valid(self, form):
-        print("asdsadsa")
         user = form.save()
         user.is_active = False
         token = secrets.token_hex(16)
@@ -35,10 +37,9 @@ class UserCreateView(CreateView):
 
         host = self.request.get_host()
         url = f"http://{host}/users/email-confirm/{token}"
-        print(url)
         send_mail(
-            subject="Тема письма",
-            message=f"Перейдите по ссылке {url}",
+            subject="Добро пожаловать в наш сервис",
+            message=f"Чтобы подтвердить почту, перейдите по ссылке {url}",
             from_email=EMAIL_HOST_USER,
             recipient_list=[user.email]
         )
@@ -50,3 +51,25 @@ def email_verification(request, token):
     user.is_active = True
     user.save()
     return redirect(reverse("catalog:product_list"))
+
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'product_form.html'
+    success_url = reverse_lazy('product_list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'product_form.html'
+    success_url = reverse_lazy('product_list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
